a7845216e3dce48775a6f30cdcd4fd0a39ab4946
Rodrigo Manhães <rmanhaes@gmail.com>
2011-09-30 18:30:00 -0300

    retrieves only the last unread feed (closes #1)

--- getfeed/__init__.py
+++ getfeed/__init__.py
@@ -17,22 +17,24 @@ class GetFeed(object):
 
     def __init__(self, url):
         self.url = url
+        self.last_received_id = None
 
     def get_new_feed(self):
         rss = feedparser.parse(self.url)
-        title = rss.entries[0].title
-        feed = Feed(title)
-        return feed
+        titles = []
+        if len(rss.entries) > 0 and rss.entries[0].id != self.last_received_id:
+            entry = rss.entries[0]
+            self.last_received_id = entry.id
+            return Feed(entry.title)
 
 
 def main():
     sched = Scheduler()
-    @sched.interval_schedule(seconds=10)
+    getfeed = GetFeed("http://planetansi.heroku.com/feeds/githubs")
+    @sched.interval_schedule(seconds=2)
     def show_last_feed():
-        getfeed = GetFeed("http://planetansi.heroku.com/feeds/githubs")
-        feed = getfeed.get_new_feed()
-        if feed:
-            feed.show()
+        feeds = getfeed.get_new_feed()
+        feeds.show()
     sched.start()
     while sched.running:
         pass
--- spec/test_getfeed.py
+++ spec/test_getfeed.py
@@ -10,11 +10,51 @@ class TestGetFeed(unittest.TestCase):
     def it_gets_a_feed_from_a_url(self):
         rss = feedparser.FeedParserDict()
         feed = feedparser.FeedParserDict()
+        feed.id = 'anything'
         feed.title = "Feed title"
         rss.entries = [feed]
         with Stub() as parse:
             from feedparser import parse
             parse("url") >> rss
         getfeed = GetFeed("url")
-        feed = getfeed.get_new_feed()
-        feed.title |should| equal_to("Feed title")
+        feeds = getfeed.get_new_feed()
+        feeds.title |should| equal_to("Feed title")
+
+    def it_retrieves_last_unread_feeds(self):
+        with Stub() as feed1: feed1.id >> 1; feed1.title >> '1'
+        with Stub() as feed2: feed2.id >> 2; feed2.title >> '2'
+        with Stub() as feed3: feed3.id >> 3; feed3.title >> '3'
+
+        with Stub() as parse:
+            from feedparser import parse
+
+            rss = feedparser.FeedParserDict()
+            rss.entries = [feed1]
+            parse("url") >> rss
+
+            rss = feedparser.FeedParserDict()
+            rss.entries = [feed2, feed1]
+            parse("url") >> rss
+
+            rss = feedparser.FeedParserDict()
+            rss.entries = [feed3, feed2, feed1]
+            parse("url") >> rss
+
+            rss = feedparser.FeedParserDict()
+            rss.entries = [feed3, feed2, feed1]
+            parse("url") >> rss
+
+        getfeed = GetFeed('url')
+
+        received_feeds = getfeed.get_new_feed()
+        received_feeds.title |should| equal_to('1')
+
+        received_feeds = getfeed.get_new_feed()
+        received_feeds.title |should| equal_to("2")
+
+        received_feeds = getfeed.get_new_feed()
+        received_feeds.title |should| equal_to("3")
+
+        received_feeds = getfeed.get_new_feed()
+        received_feeds |should| be(None)
+
